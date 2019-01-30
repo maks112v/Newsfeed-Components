d3cfe0c8cfada5dcb532fc578c4f7a84bd687072
maks112v <maks112v@gmail.com>
2019-01-30 16:48:02 -0500

    added Icons

--- Article/Article.js
+++ Article/Article.js
@@ -7,13 +7,15 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.icon = domElement.querySelector('.fas');
+    //this.expandButton.innerHtml = "";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.domElement.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.icon.classList.toggle('fa-rotate-180');
     this.domElement.classList.toggle('article-open');
   }
 }
